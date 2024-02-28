b5b2d00e9fb4bcd1396df63189b44dc931de0a1e
GodieG-05 <diegogalanm@gmail.com>
2024-02-27 20:53:04 -0500

    feat: :art: Eliminacion de estructura HTML y carga dinamica de elementos con JS
    Se crean elementos dinamicamente con JS para contener la estructura HTML eliminada

---
+++ js/app.js
@@ -0,0 +1,218 @@
+const dcHeros = [
+    {
+        "name" : "BATMAN",
+        "picture" : "https://majorspoilers.com/wp-content/uploads/2019/12/BATMAN_Cv86-scaled.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "SUPERMAN",
+        "picture" : "https://i.pinimg.com/originals/82/41/5d/82415d90ebfd1b84c348088f6d09e5c2.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "AQUAMAN",
+        "picture" : "https://www.comicartcommunity.com/gallery/data/media/242/AQUAMAN_7.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "FLASH",
+        "picture" : "https://oyster.ignimgs.com/wordpress/stg.ign.com/2014/04/STK6353251.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "LINTERNA VERDE",
+        "picture" : "https://www.gamespot.com/a/uploads/original/1562/15626911/3079590-hjglc_1_02_color_rev4.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "JOKER",
+        "picture" : "https://mascarillasnanocobre.cl/wp-content/uploads/2020/07/dc-marvel-bocas-93-scaled.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "DARKSEID",
+        "picture" : "https://rare-gallery.com/thumbnail/1404113-darkseid-superheroes-hd-4k-5k-artist-artwork.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "DEATHSTROKE",
+        "picture" : "https://w0.peakpx.com/wallpaper/328/892/HD-wallpaper-deathstroke-slade-dc.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "BLACKADAM",
+        "picture" : "https://wallpapercave.com/wp/wp9622782.png",
+        "about" : ""
+    },
+    {
+        "name" : "PEACEMAKER",
+        "picture" : "https://e0.pxfuel.com/wallpapers/832/617/desktop-wallpaper-peacemaker-in-2022-dc-comics-superhero-comics.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "MUJER MARAVILLA",
+        "picture" : "https://wallpapers-clan.com/wp-content/uploads/2023/02/dc-wonder-woman-art-wallpaper.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "CYBORG",
+        "picture" : "https://e1.pxfuel.com/desktop-wallpaper/973/977/desktop-wallpaper-pin-on-comics-cyborg-dc-comics.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "RAVEN",
+        "picture" : "https://e0.pxfuel.com/wallpapers/63/524/desktop-wallpaper-raven-dc-raven-dc-comics.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "HARLEY QUINN",
+        "picture" : "https://c4.wallpaperflare.com/wallpaper/206/333/254/harley-quinn-women-comic-art-comics-dc-comics-hd-wallpaper-preview.jpg",
+        "about" : ""
+    }
+]
+
+const marvelHeros = [
+    {
+        "name" : "IRON MAN",
+        "picture" : "https://e0.pxfuel.com/wallpapers/716/542/desktop-wallpaper-artstation-ironman-mark-46-egor-rumyantcev-thumbnail.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "SPIDERMAN",
+        "picture" : "https://e0.pxfuel.com/wallpapers/785/111/desktop-wallpaper-the-amazing-spider-man-2-2014-phone-moviemania-spiderman-artwork-amazing-spiderman-movie-marvel-spiderman-art-spider-man-amazing-thumbnail.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "HULK",
+        "picture" : "https://e0.pxfuel.com/wallpapers/208/947/desktop-wallpaper-hulk-dc-marvel-avengers-thumbnail.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "THOR",
+        "picture" : "https://e0.pxfuel.com/wallpapers/291/600/desktop-wallpaper-thor-ideas-thor-the-mighty-thor-marvel-comics-thor-comic-art-thumbnail.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "PANTERA NEGRA",
+        "picture" : "https://e0.pxfuel.com/wallpapers/850/896/desktop-wallpaper-black-panther-in-2020-marvel-comics-black-panther-marvel-black-panther-movie-poster-neon-panther-thumbnail.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "VENOM",
+        "picture" : "https://e0.pxfuel.com/wallpapers/369/143/desktop-wallpaper-venom-american-comic-character-thumbnail.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "DOCTOR STRANGE",
+        "picture" : "https://e0.pxfuel.com/wallpapers/863/668/desktop-wallpaper-doctor-strange-thor-doctor-strange-comics-avengers-marvel-comicon-thumbnail.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "THANOS",
+        "picture" : "https://e0.pxfuel.com/wallpapers/174/455/desktop-wallpaper-thanos-desenhos-marvel-avengers-and-comics-galactus-thumbnail.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "DEADPOOL",
+        "picture" : "https://www.enwallpaper.com/wp-content/uploads/2021/11/www.mobilesmspk.net-deadpool-4419.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "CAPITÁN AMÉRICA",
+        "picture" : "https://e0.pxfuel.com/wallpapers/554/665/desktop-wallpaper-captain-america-captain-america-super-hero-thumbnail.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "WOLVERINE",
+        "picture" : "https://e0.pxfuel.com/wallpapers/610/46/desktop-wallpaper-wolverine-comic-art-thumbnail.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "PUNISHER",
+        "picture" : "https://e0.pxfuel.com/wallpapers/599/987/desktop-wallpaper-the-punisher-marvel-the-punisher-comics-thumbnail.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "DOCTOR DOOM",
+        "picture" : "https://e1.pxfuel.com/desktop-wallpaper/52/237/desktop-wallpaper-dr-doom-doctor-doom-marvel-comics-thumbnail.jpg",
+        "about" : ""
+    },
+    {
+        "name" : "DAREDEVIL",
+        "picture" : "https://e0.pxfuel.com/wallpapers/863/715/desktop-wallpaper-marco-checchetto-on-twitter-daredevil-comic-marvel-daredevil-marvel-comics-art-daredevil-comics-thumbnail.jpg",
+        "about" : ""
+    }
+]
+
+
+const dcContainer = document.getElementById("dc__heros");
+
+dcHeros.forEach(hero =>{
+
+    //Crear el elemento div que va a contener la tarjeta de cada heroe
+    const gridItem = document.createElement('div')
+    gridItem.classList.add('grid-item');
+
+    //Crear el elemento img
+    const img = document.createElement('img');
+    img.src = hero.picture;
+    img.alt = hero.name;
+
+    //Crear el elemento div que contiene el nombre y el boton del heroe
+    const infoDiv = document.createElement('div');
+    infoDiv.classList.add('grid-item__info')
+
+    //Crear los elementos p para el nombre y el boton
+    const nameP = document.createElement('p');
+    nameP.textContent = hero.name;
+
+    const botonP = document.createElement('button');
+    botonP.textContent = 'VER';
+
+    //Agregar los elementos p a infoDiv
+    infoDiv.appendChild(nameP);
+    infoDiv.appendChild(botonP);
+
+    //Agregar los elementos img e infoDiv a al div grid-item
+    gridItem.appendChild(img);
+    gridItem.appendChild(infoDiv);
+
+    //Agregar el grid-item a dcContainer 'dc__heros'
+    dcContainer.appendChild(gridItem);
+})
+
+const marvelContainer = document.getElementById("marvel__heros");
+
+marvelHeros.forEach(hero =>{
+
+    //Crear el elemento div que va a contener la tarjeta de cada heroe
+    const gridItem = document.createElement('div')
+    gridItem.classList.add('grid-item');
+
+    //Crear el elemento img
+    const img = document.createElement('img');
+    img.src = hero.picture;
+    img.alt = hero.name;
+
+    //Crear el elemento div que contiene el nombre y el boton del heroe
+    const infoDiv = document.createElement('div');
+    infoDiv.classList.add('grid-item__info')
+
+    //Crear los elementos p para el nombre y el boton
+    const nameP = document.createElement('p');
+    nameP.textContent = hero.name;
+
+    const botonP = document.createElement('button');
+    botonP.textContent = 'VER';
+
+    //Agregar los elementos p a infoDiv
+    infoDiv.appendChild(nameP);
+    infoDiv.appendChild(botonP);
+
+    //Agregar los elementos img e infoDiv a al div grid-item
+    gridItem.appendChild(img);
+    gridItem.appendChild(infoDiv);
+
+    //Agregar el grid-item a dcContainer 'dc__heros'
+    marvelContainer.appendChild(gridItem);
+})
